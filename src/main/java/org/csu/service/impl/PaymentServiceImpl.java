b1437135f24ec84d4afee02d99140bf3003fb3ac
LeanneTN <2321604632@qq.com>
2022-07-06 01:09:15 +0800

    fix bug of paybyId and finish the function of paying one's all bill

--- src/main/java/org/csu/service/impl/PaymentServiceImpl.java
+++ src/main/java/org/csu/service/impl/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package org.csu.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import org.csu.domain.Payment;
 import org.csu.vo.ResponseCode;
 import org.csu.vo.ResponseResult;
@@ -8,24 +10,39 @@ import org.csu.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PaymentServiceImpl implements PaymentService {
     @Autowired
     private PaymentMapper paymentMapper;
 
     @Override
-    public ResponseResult pay(Long id) {
-        return null;
+    public ResponseResult pay(Long uid) {
+        QueryWrapper<Payment> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("fromUser", uid);
+        queryWrapper.eq("if_paid", false);
+        List<Payment> paymentList = paymentMapper.selectList(queryWrapper);
+        UpdateWrapper<Payment> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.eq("if_paid", false).set("if_paid", true);
+        paymentMapper.update(null,updateWrapper);
+        return new ResponseResult(ResponseCode.SUCCESS.getCode(), "用户所有订单支付成功",paymentList);
     }
 
     @Override
     public ResponseResult payById(Long id, Payment payment) {
         Payment payment1 = paymentMapper.selectById(id);
-        if(payment1!= null){
-            return new ResponseResult(ResponseCode.PAYMENT_EXIST.getCode(), "订单已存在", payment1);
+        if(payment1!= null&& payment1.isIf_paid()){
+            return new ResponseResult(ResponseCode.PAYMENT_EXIST.getCode(), "订单已支付", payment1);
+        }else if(payment1!=null&&!payment1.isIf_paid()){
+            payment.setIf_paid(true);
+            paymentMapper.updateById(payment);
+            return new ResponseResult(ResponseCode.SUCCESS.getCode(), "支付成功", payment);
+        }else if(payment1==null) {
+            paymentMapper.insert(payment);
+            return new ResponseResult(ResponseCode.SUCCESS.getCode(), "支付成功", payment);
         }
-        paymentMapper.insert(payment);
-        return new ResponseResult(ResponseCode.SUCCESS.getCode(), "支付成功", payment);
+        return new ResponseResult(ResponseCode.ERROR.getCode(), "支付失败");
     }
 
     @Override
